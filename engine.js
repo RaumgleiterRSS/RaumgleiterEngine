1b282ebbb81a9a410abd47a489ddfcb30d5ea9ee
Daniel Henning <daniel@puresick.me>
2017-05-01 23:36:42 +0200

    multiple sources are now output as one json

--- engine.js
+++ engine.js
@@ -1,11 +1,9 @@
 const http = require('http')
 const https = require('https')
-const fs = require('fs')
 const xml2js = require('xml2js')
+const config = require('./config.json')
 
-const port = 3000
-
-let s1 = 'https://www.computerbase.de/rss/news.xml'
+const port = config.system.server.port
 
 function fetchSourceFeed (source, callback) {
 	https.get(source, (res) => {
@@ -29,19 +27,45 @@ function convertXmlToJson (xml, callback) {
 	})
 }
 
+function convertIdToValidJsonKey (id) {
+	const string = JSON.stringify(id[0])
+	const strippedId = string
+		.replace('"', '')
+		.replace(/https:\/\//, '')
+		.replace(/http:\/\//, '')
+		.replace('www.', '')
+		.replace(/\/.+$/, '')
+		.replace('.', '_')
+	return strippedId
+}
+
 //creating as https server?!
 http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain'})
-	fetchSourceFeed(s1, (payload) => {
-		convertXmlToJson(payload, (json) => {
-			console.log(json.feed.entry[0].title)
-			//needs to be only sent on / not on /favicon
-			console.log(req.url)
-			let jsonAsString = JSON.stringify(json)
-			res.write(jsonAsString)
-			res.end()
+
+	let completeJsonPayload = {}
+	const sources = config.sources
+
+	//for (let singleSource of config.sources) {
+	for (let index = 0; index < sources.length; index++) {
+		fetchSourceFeed(sources[index], (payload) => {
+			convertXmlToJson(payload, (json) => {
+				//needs to be only sent on / not on /favicon
+				//console.log(req.url)
+				const jsonAsString = JSON.stringify(json)
+				const jsonKey = convertIdToValidJsonKey(json.feed.id)
+
+				completeJsonPayload[jsonKey] = json
+
+				//check if it is the last item checked beforen the loop stops
+				//and send ending response
+				if (index == sources.length -1) {
+					res.write(JSON.stringify(completeJsonPayload))
+					res.end()
+				}
+			})
 		})
-	})
+	}
 }).listen(port)
 
 console.log(`Engine running on port ${port}`)
